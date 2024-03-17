8e96418eda5096ea21e798a74feebad100db93f4
Abolfazl Mohammadi <abmoh.1379@gmail.com>
2024-03-17 19:26:00 +0330

    finished designing notFoundPage

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LayoutContainer from "../components/UI/LayoutContainer";
+import MainElement from "../components/UI/MainElement";
+import NotFound from '../components/app/NotFound';
 
 const NotFoundPage = () => {
   return (
-    <React.Fragment>
-      <p>
-        The page you are trying to visit, seems to not exist on our server. if
-        you feel otherwise, please feel free to contact our support staff.
-      </p>
-      <Link to= '/'>Go Home</Link>
-    </React.Fragment>
+    <div className="not-found">
+      <header className="not-found__header">
+        <LayoutContainer className="container--not-found-header">
+          <p className="not-found__title">
+            <Link className="not-found__title-link" to="/" replace = {true}>
+              Budmoney
+            </Link>
+          </p>
+        </LayoutContainer>
+      </header>
+      <MainElement>
+        <LayoutContainer className= 'container--not-found-main'>
+          <NotFound />
+        </LayoutContainer>
+      </MainElement>
+    </div>
   );
 };
 
